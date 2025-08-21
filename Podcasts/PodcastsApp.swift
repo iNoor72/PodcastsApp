7bd0455f60e506f4cacb96a2ae914068b6a761e6
Noor El-Din Walid <noorwalid72@yahoo.com>
2025-08-21 18:57:21 +0300

    Initial Commit

---
+++ Podcasts/PodcastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastsApp.swift
+//  Podcasts
+//
+//  Created by Noor El-Din Walid on 21/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PodcastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
